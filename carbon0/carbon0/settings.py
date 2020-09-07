3b9391986456c419c133ad4b15f5a02e23461140
= <zainr7989@gmail.com>
2020-09-07 15:49:14 -0400

    Tweak settings for deployment on Heroku.

--- carbon0/carbon0/settings.py
+++ carbon0/carbon0/settings.py
@@ -10,22 +10,33 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+from dotenv import load_dotenv
+import django_heroku
+import dj_database_url
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Loads in environment variables from a .env file
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0xwex3z56-9s_!r-423lf0f^)7ot8c46$dv)&#^8a!10&5d%46'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'carbon0.herokuapp.com',
+]
 
 
 # Application definition
@@ -75,8 +86,12 @@ WSGI_APPLICATION = 'carbon0.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carbon0',
+        'USER': 'postgres',
+        'PASSWORD': str(os.getenv('DATABASE_PASSWORD')),
+        'HOST': '',
+        'PORT': '5432',
     }
 }
 
@@ -118,3 +133,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# where to find static files in production
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+# Convert the DATABASE_URL environment variable into what Django understands
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
+# Additional support setting up env for Heroku
+django_heroku.settings(locals())
