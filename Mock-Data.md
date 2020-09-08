b57c68f5a103c82972baa93f0be1d3b2588c2655
= <zainr7989@gmail.com>
2020-09-08 18:55:01 -0400

    Add dummy model data, edit models.

---
+++ Mock-Data.md
@@ -0,0 +1,31 @@
+# Mock Data for Models
+
+Use the screenshots below to guide you as you make the dummy instances of the Question and Mission models on your local environment.
+
+You will need to create a super user account on your local version of the project, and then you can create the models just from the Admin Dashboard.
+
+## Question Model
+
+1. Recycling Question:
+
+![Screenshot 1](https://i.postimg.cc/HW2wMgkt/Screen-Shot-2020-09-08-at-6-36-09-PM.png)
+
+2. Transit Question:
+
+![Screenshot 2](https://i.postimg.cc/289nH7vb/Screen-Shot-2020-09-08-at-6-47-16-PM.png)
+
+3. Diet Question:
+
+![Screenshot 3](https://i.postimg.cc/Wb1M6HDc/Screen-Shot-2020-09-08-at-6-47-24-PM.png)
+
+4. Air Travel Question:
+
+![Screenshot 4](https://i.postimg.cc/RZ3K2JQP/Screen-Shot-2020-09-08-at-6-47-31-PM.png)
+
+5. Utilities Question:
+
+![Screenshot 5](https://i.postimg.cc/13LwSRvy/Screen-Shot-2020-09-08-at-6-47-35-PM.png)
+
+## Mission Model
+
+TBD
--- carbon0/carbon_quiz/models.py
+++ carbon0/carbon_quiz/models.py
@@ -6,10 +6,10 @@ from django.utils import timezone
 
 class Question(models.Model):
     '''Represents a single question on the Carbon calculator quiz.'''
-    question_text = models.CharField(max_length=200,
+    question_text = models.CharField(max_length=500,
         help_text="Question for the user"
     )
-    question_info = models.CharField(max_length=200,
+    question_info = models.TextField(
         help_text="Explains any vocabulary relevant to the question."
     )
     carbon_value = models.FloatField(
@@ -26,6 +26,14 @@ class Question(models.Model):
     category = models.CharField(max_length=1, choices=CATEGORIES,
         help_text="The area of sustainability to which this question relates."
     )
+    learn_more_link = models.CharField(max_length=1000, help_text=
+        "Hyperlink where the user can learn more about the question",
+        null=True, blank=True
+    )
+
+    def __str__(self):
+        '''Returns the category of the Question, and it's id.'''
+        return f'Question {self.category} {self.id}'
 
 
 class Quiz(models.Model):
