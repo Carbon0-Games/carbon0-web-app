68b44a612d911d4c9a24b7b760340ba94702af7a
= <zainr7989@gmail.com>
2020-09-09 10:12:53 -0400

    Add view controller for QuizCreate.

--- carbon0/carbon_quiz/urls.py
+++ carbon0/carbon_quiz/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 
+from .views import (
+    QuizCreate,
+)
+
 app_name = 'carbon_quiz'
 
-urlpatterns = []
+urlpatterns = [
+    path('create-quiz/', QuizCreate.as_view(), name='quiz_create'),
+]
--- carbon0/carbon_quiz/views.py
+++ carbon0/carbon_quiz/views.py
@@ -21,3 +21,24 @@ from .models import (
 class QuizCreate(CreateView):
     '''View to create new Quiz instance from randomly picked questions.'''
     model = Quiz
+    fields = []
+
+    def generate_random_question(self, category):
+        '''Gets a Question model in a specific category randomly.'''
+        category_questions = Question.objects.filter(category=category)
+        return random.sample(category_questions, 1)
+
+    def form_valid(self, form):
+        '''Initializes the Questions the user will answer on the Quiz.'''
+        # get random questions
+        quiz_questions = list()
+        for category in Question.CATEGORIES:
+            # get the value stored for the category field on the model
+            category_value, category_full_name = category
+            # get a Question instance in that category
+            next_question = self.generate_random_question(category_value)
+            # add it to the list
+            quiz_questions.append(next_question)
+        # set the questions list on the model
+        form.instance.questions = quiz_questions
+        return super().form_valid(form)
