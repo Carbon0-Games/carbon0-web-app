8e515d50d90583981eafed0d7401096804d5e3bd
= <zainr7989@gmail.com>
2020-09-09 10:01:50 -0400

    Add QuizForm class.

--- carbon0/carbon_quiz/views.py
+++ carbon0/carbon_quiz/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+import random
 
-# Create your views here.
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse, reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import (
+    CreateView,
+    UpdateView,
+    DeleteView)
+
+from .models import (
+    Achievement,
+    Mission,
+    Question,
+    Quiz,
+)
+
+
+class QuizCreate(CreateView):
+    '''View to create new Quiz instance from randomly picked questions.'''
+    model = Quiz
