beedf5f98a95e29df93c6da0524646883d0c6a35
= <zainr7989@gmail.com>
2020-10-04 10:01:53 -0400

    Code cleanup.

--- carbon0/api/views.py
+++ carbon0/api/views.py
@@ -4,7 +4,6 @@ from django.urls import reverse, reverse_lazy
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from carbon_quiz.models.question import Question
 from carbon_quiz.models.quiz import Quiz
 
 
