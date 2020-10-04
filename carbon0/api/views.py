a78943e601443f61312296302ee11fbff768fbb8
= <zainr7989@gmail.com>
2020-10-03 21:08:09 -0400

    Init QuizUpdate.get (API endpoint).

--- carbon0/api/views.py
+++ carbon0/api/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -9,3 +10,20 @@ class QuizUpdate(APIView):
     Updates the questions that have been answered yes/no in the Quiz,
     and then moves to the next template.
     """
+    def get(self, request, quiz_slug, response_to_question):
+        """
+        Uses the answer to the Question the user has just responded to,
+        to update the array of questions related to a Quiz.
+
+        Parameters:
+        request(HttpRequest)
+        quiz_slug(str): the unique slug value of one of the Quizzes
+        response_to_question(int): 0 or 1, which means the user responded
+                                   no or yes (respectively)
+
+        Returns:
+        HttpResponse: a view of the QuizDetail, with the next question, 
+                      or missions for the user
+
+        """
+        pass
