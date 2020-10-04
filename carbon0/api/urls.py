b1792c400692654673a35929b218f2351b681928
= <zainr7989@gmail.com>
2020-10-03 21:00:55 -0400

    Add AJAX request to API from QuizDetail template.

--- carbon0/api/urls.py
+++ carbon0/api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from api.views import QuizUpdate
 
 urlpatterns = [
-    path('next-question/<int:quiz_id>/<int:response_to_question>/',
+    path('next-question/<slug:quiz_slug>/<int:response_to_question>/',
          QuizUpdate.as_view(), name="quiz_update"),
 ]
