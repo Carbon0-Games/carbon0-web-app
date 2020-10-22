065769d2da9ce95e4103ad7e79537055a636d9db
= <zainr7989@gmail.com>
2020-10-22 17:34:01 -0400

    Type cast DEBUG as a boolean.

--- carbon0/carbon0/settings.py
+++ carbon0/carbon0/settings.py
@@ -29,7 +29,7 @@ load_dotenv()
 SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = os.getenv("DEBUG", False)
 
 ALLOWED_HOSTS = [
     "localhost",
