7410c2cf62729093399504b0545988f473c45f00
Cao Mai <caomai@rocketmail.com>
2020-10-22 08:45:23 -0700

    Update barChartFunction.js

--- carbon0/static/js/barChartFunction.js
+++ carbon0/static/js/barChartFunction.js
@@ -1,33 +1,35 @@
 // make bar chart
 export function barChart(plotLabels, plotData) {
-    let ctx = document.getElementById('barChart').getContext('2d');
-    let barChart = new Chart(ctx, {
-        type: "horizontalBar",
-        data: {
-            datasets: [{
-                label: 'metric tons CO2',
-                data: plotData,
-                // set options for the chart
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                ],
-                borderWidth: 1
-            }],
-            labels: plotLabels
+  let ctx = document.getElementById("barChart").getContext("2d");
+  let barChart = new Chart(ctx, {
+    type: "horizontalBar",
+    data: {
+      datasets: [
+        {
+          label: "metric tons CO2",
+          data: plotData,
+          // set options for the chart
+          backgroundColor: ["rgba(255, 99, 132, 0.2)"],
+          borderColor: ["rgba(255, 99, 132, 1)"],
+          borderWidth: 1,
         },
-        options: {
-            responsive: true,
-            scales: {
-                yAxes: [{
-                    display: true,
-                    ticks: {
-                        beginAtZero: true,
-                    }
-                }]
-            }
-        }
-    });
+      ],
+      labels: plotLabels,
+    },
+    options: {
+      responsive: true,
+      scales: {
+        xAxes: [
+          {
+            display: true,
+            ticks: {
+              max: 1020,
+              min: 995,
+              // beginAtZero: true,
+            },
+          },
+        ],
+      },
+    },
+  });
 }
