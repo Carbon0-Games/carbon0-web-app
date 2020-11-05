e46d2fc792781dd3671157275a7e6f6f8e21a4f0
Cao Mai <caomai@rocketmail.com>
2020-11-04 21:01:49 -0800

    Add configurations for bar and guage charts

--- carbon0/static/js/barChartFunction.js
+++ carbon0/static/js/barChartFunction.js
@@ -2,7 +2,8 @@
 export function barChart(plotLabels, plotData) {
   let ctx = document.getElementById("barChart").getContext("2d");
 
-  var config = {
+  // configuration for gauge chart
+  const configGaugeChart = {
     type: 'gauge',
     data: {
       //labels: ['Success', 'Warning', 'Warning', 'Error'],
@@ -35,7 +36,8 @@ export function barChart(plotLabels, plotData) {
         color: 'rgba(0, 0, 0, 1)'
       },
       valueLabel: {
-        formatter: Math.round
+        formatter: Math.round,
+        display: true,
       },
       plugins: {
         datalabels: {
@@ -58,52 +60,56 @@ export function barChart(plotLabels, plotData) {
     }
   };
 
-  new Chart(ctx, config);
+// configuration for horizontal bar chart
+  const configBarChart = {
+    type: "horizontalBar",
+    data: {
+      datasets: [
+        {
+          label: "metric tons CO2",
+          data: plotData,
+          // set options for the chart
+          backgroundColor: ["rgba(255, 99, 132, 0.2)"],
+          borderColor: ["rgba(255, 99, 132, 1)"],
+          borderWidth: 1,
+        },
+      ],
+      labels: plotLabels,
+    },
+    options: {
+      responsive: true,
+      scales: {
+        xAxes: [{
+          display: true,
+          ticks: {
+            max: 20000,
+            beginAtZero: true,
+            // turn the tick markers white
+            fontColor: 'rgb(255, 255, 255)'
+          },
+          gridLines: {
+            color: "white"
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            // color the plot label white
+            fontColor: "white",
+          }
+        }],
+      },
+      // turn the legend white
+      legend: {
+        display: true,
+        labels: {
+          fontColor: 'rgb(255, 255, 255)',
+        }
+      },
+    },
+  };
 
-  // let barChart = new Chart(ctx, {
-  //   type: "horizontalBar",
-  //   data: {
-  //     datasets: [
-  //       {
-  //         label: "metric tons CO2",
-  //         data: plotData,
-  //         // set options for the chart
-  //         backgroundColor: ["rgba(255, 99, 132, 0.2)"],
-  //         borderColor: ["rgba(255, 99, 132, 1)"],
-  //         borderWidth: 1,
-  //       },
-  //     ],
-  //     labels: plotLabels,
-  //   },
-  //   options: {
-  //     responsive: true,
-  //     scales: {
-  //       xAxes: [{
-  //         display: true,
-  //         ticks: {
-  //           max: 20000,
-  //           beginAtZero: true,
-  //           // turn the tick markers white
-  //           fontColor: 'rgb(255, 255, 255)'
-  //         },
-  //         gridLines: {
-  //           color: "white"
-  //         }
-  //       }],
-  //       yAxes: [{
-  //         ticks: {
-  //           // color the plot label white
-  //           fontColor: "white",
-  //         }
-  //       }],
-  //     },
-  //     // turn the legend white
-  //     legend: {
-  //       display: true,
-  //       labels: {
-  //         fontColor: 'rgb(255, 255, 255)',
-  //       }
-  //     },
-  //   },
-  // });
+// to enable either gauge or horizontal bar chart; either or and not both!
+new Chart(ctx, configGaugeChart)
+// new Chart(ctx, configBarChart)
+
 }
