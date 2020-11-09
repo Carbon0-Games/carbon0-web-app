868abe7488c79c38ce0296d3d7df7c8281d20ee5
= <zainr7989@gmail.com>
2020-11-09 09:21:08 -0500

    Edit SECRET_KEY and ALLOWED_HOSTS for loca/prod settings.

--- carbon0/carbon0/settings/local.py
+++ carbon0/carbon0/settings/local.py
@@ -5,3 +5,10 @@ DEBUG = True
 
 # Mixpanel Project Token
 MP_PROJECT_TOKEN = ''
+
+SECRET_KEY = '84#be86m-0ud#g$m&#6^)hd!$qyhl6mw73nm11$)=jufb-cw+4'
+
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+]
