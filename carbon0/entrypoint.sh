b7877d65c83de54a4a90f833a5f8ba6794c733cf
= <zainr7989@gmail.com>
2020-11-17 12:28:05 -0500

    Add loaddate command to entrypoint.sh.

--- carbon0/entrypoint.sh
+++ carbon0/entrypoint.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
+
+# Credit to Mausam Gaurav, who posted the Dockerfile I used as a starting point
+# on this blog: 
+# https://datagraphi.com/blog/post/2020/8/30/docker-guide-build-a-fully-production-ready-machine-learning-app-with-react-django-and-postgresql-on-docker
+
+# make sure PostgreSQL is running before we migrate data and SQL schema
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $DB_HOST $DB_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+# migrate data and SQL schema
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-
+python manage.py loaddata carbon_quiz/fixtures/mission_link_data.json carbon_quiz/fixtures/question_data.json
