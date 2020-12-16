54fdc862d3365227f0362a032f3dbdc7a196fc17
= <zainr7989@gmail.com>
2020-12-16 13:16:38 -0500

    Add test command to entrypoint script.

--- carbon0/entrypoint.sh
+++ carbon0/entrypoint.sh
@@ -22,5 +22,7 @@ export DJANGO_SETTINGS_MODULE=carbon0.settings.local
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 python manage.py loaddata carbon_quiz/fixtures/mission_link_data.json carbon_quiz/fixtures/question_data.json
-# python manage.py runserver 0.0.0.0:8000
+# The test command has been commented out - turn it back on whenever you specifically want
+# to run the tests within Docker!
+# python manage.py test
 gunicorn carbon0.wsgi:application --bind 0.0.0.0:8000
