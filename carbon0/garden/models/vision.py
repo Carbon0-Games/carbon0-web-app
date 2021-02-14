ea7e2f91f53b8b50a41260a9dcbea83bb932fa6d
= <zainr7989@gmail.com>
2021-02-14 14:19:01 -0500

    Implement Vision model.

--- carbon0/garden/models/vision.py
+++ carbon0/garden/models/vision.py
@@ -1,1 +1,17 @@
 from django.db import models
+import tensorflow.keras.models as keras_models
+
+
+class Vision(models.Model):
+    architecture = models.FileField(
+        upload_to="neural_networks/architecture/",
+        null=True, 
+        help_text="JSON instructions for how to constrcut \
+                  the underlying neural network."
+    )
+    weights = models.FileField(
+        upload_to="neural_networks/parameters/",
+        null=True, 
+        help_text="Hadoop instructions for what weights and biases \
+                  to give the underlying neural network."
+    )
