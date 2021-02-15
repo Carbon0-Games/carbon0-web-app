decd7cfadfbe8763c3903af0229aad1c611ea0f6
= <zainr7989@gmail.com>
2021-02-15 11:14:32 -0500

    Implement displayHealthPreview function.

--- carbon0/static/js/healthPreview.js
+++ carbon0/static/js/healthPreview.js
@@ -1,7 +1,31 @@
 // show when this plant was last updated, and what the status was
 export function displayHealthPreview(elemID, lastStatus, lastUpdated) {
     // get the HTML element we'll be making changes to
-    let ctx = document.getElementById(elemID);
-    // TODO: show the last time the plant was updated on the DOM
-    // TODO: do the same for the latest health check result
+    let parentElem = document.getElementById(elemID);
+        // add an element for the latest health check
+    let health = document.createElement("p");
+    const healthNode = document.createTextNode(lastStatus);
+    health.appendChild(healthNode);
+    // add Bootstrap classes to the time element
+    let textColor = "";
+    if (lastStatus === "Healthy") {
+        textColor = "text-success";
+    } else if (lastStatus === "Unhealthy") {
+        textColor = "text-danger";
+    } else if (lastStatus === "Unhealthy") {
+        textColor = "text-warning";
+    }
+    healt.classList.add("card-text");
+    health.classList.add(textColor);
+    // add the health to the HTML document
+    parentElem.appendChild(health);
+    // show the last time the plant was updated on the DOM
+    let time = document.createElement("p");
+    const timeNode = document.createTextNode("Last Updated: " + lastUpdated);
+    time.appendChild(timeNode);
+    // add Bootstrap classes to the time element
+    time.classList.add("card-text");
+    time.classList.add("text-dark");
+    // add the time to the HTML document
+    parentElem.appendChild(time);
 }
