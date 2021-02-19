94ac0f4936e1723ecebab361baeca1f32f376375
= <zainr7989@gmail.com>
2021-02-19 09:56:58 -0500

    Implement controller func for PlantList.

--- carbon0/garden/views.py
+++ carbon0/garden/views.py
@@ -1,7 +1,25 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views.generic import ListView
 
+from .models.plant import Plant
+
 
 class PlantList(ListView):
-    def get(self):
-        pass
+
+    model = Plant
+    queryset = Plant.objects.all()
+    template_name = "garden/plant_list.html"
+
+    def get(self, request: HttpRequest) -> HttpResponse:
+        """Where the user can see all their registered plants.
+        
+        Parameters:
+        request(HttpRequest): the GET request sent by the client
+
+        Response: HttpResponse: the view of the template
+        """
+        # define the context
+        context = {"plants": self.get_queryset()}
+        # return the response
+        return render(request, self.template_name, context)
