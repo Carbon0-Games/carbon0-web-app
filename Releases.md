4f3e9abf3d17bef4593a907e9bf4c578deb1d1aa
= <zainr7989@gmail.com>
2021-02-24 10:37:16 -0500

    Update notebook link, README tech stack, and making prediction in MachineLearning.

--- Releases.md
+++ Releases.md
@@ -14,4 +14,4 @@ These releases are tenatively scheduled. **Thank you for your patience** if some
 
 ## Release Notes
 
-1. *Plant Vision*: the convolutional neural network for this feature was developed using transfer learning. The full implementation is available [in this Google Colab notebook](https://colab.research.google.com/drive/1NXIpfOntcUG3iVcx3tWt0Wt7Hsf5PPV7).
+1. *PlantNet*: the convolutional neural network for this feature was developed using transfer learning. The full implementation is available [in this Jupyter notebook](https://github.com/UPstartDeveloper/DS-2.4-Advanced-Topics/blob/main/Notebooks/Computer_Vision/Plant_Vision.ipynb).
--- carbon0/garden/models/ml.py
+++ carbon0/garden/models/ml.py
@@ -1,4 +1,5 @@
 from django.db import models
+import numpy as np
 from tensorflow import keras
 import tensorflow as tf
 
@@ -26,6 +27,48 @@ class MachineLearning(models.Model):
         help_text="Hadoop instructions for what weights and biases \
                   to give the underlying neural network.",
     )
+    # store a list of the classes the CNN model can label a leaf
+    LEAF_LABELS = np.array([
+        # labels were adapted from "New Plant Diseases Dataset": https://www.kaggle.com/vipoooool/new-plant-diseases-dataset
+        'Strawberry___healthy',
+        'Grape___Black_rot',
+        'Potato___Early_blight',
+        'Blueberry___healthy',
+        'Corn_(maize)___healthy',
+        'Tomato___Target_Spot',
+        'Peach___healthy',
+        'Potato___Late_blight',
+        'Tomato___Late_blight',
+        'Tomato___Tomato_mosaic_virus',
+        'Pepper__bell___healthy',
+        'Orange___Haunglongbing_(Citrus_greening)',
+        'Tomato___Leaf_Mold',
+        'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
+        'Cherry_(including_sour)___Powdery_mildew',
+        'Apple___Cedar_apple_rust',
+        'Tomato___Bacterial_spot',
+        'Grape___healthy',
+        'Tomato___Early_blight',
+        'Corn_(maize)___Common_rust_',
+        'Grape___Esca_(Black_Measles)',
+        'Raspberry___healthy',
+        'Tomato___healthy',
+        'Cherry_(including_sour)___healthy',
+        'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+        'Apple___Apple_scab',
+        'Corn_(maize)___Northern_Leaf_Blight',
+        'Tomato___Spider_mites Two-spotted_spider_mite',
+        'Peach___Bacterial_spot',
+        'Pepper,_bell___Bacterial_spot',
+        'Tomato___Septoria_leaf_spot',
+        'Squash___Powdery_mildew',
+        'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
+        'Apple___Black_rot',
+        'Apple___healthy',
+        'Strawberry___Leaf_scorch',
+        'Potato___healthy',
+        'Soybean___healthy'
+    ])
 
     def __str__(self):
         """Return a human-understandable name for the deep learning model."""
@@ -34,10 +77,10 @@ class MachineLearning(models.Model):
     def build(self):
         """Use the model fields to instantiate a neural network."""
         # Load Architecture
-        with open(self.architecture, 'r') as f:
+        with open(self.architecture.url, 'r') as f:
             model = keras.models.model_from_json(f.read())
             # Load Weights
-            model.load_weights(self.weights)
+            model.load_weights(self.weights.url)
             return model
 
     def predict_health(self, leaf):
@@ -54,10 +97,13 @@ class MachineLearning(models.Model):
                        is correct.
         """
         # build the model
+        model = self.build()
         # preprocess the image data
+        img = tf.image.resize(leaf.image, [256, 256])
+        final_image = tf.keras.applications.inception_v3.preprocess_input(img)
         # predict on the image data
-        # decide the status
-        # decide the condition
-        # add the confidence to the output
+        prediction_probabilities = model.predict(final_image)
+        # TODO: decide the status, confidence, and condition
+        confidence = np.argmax(prediction_probabilities)
         # TODO: replace dummy output below
-        return ["H", "No symptoms", 0.87]
+        return ["H", "No symptoms", confidence]
